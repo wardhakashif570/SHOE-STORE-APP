806064a205ed977da438e0beffa64407fe14c754
wardhakashif <wardhakashif570@gmail.com>
2020-08-31 15:13:49 -0700

    updated logo.js

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo=()=>
 {
 return(
-<img src={process.env.PUBLIC_URL + "/images/logo.jpg"} width="100px" height="50px" alt="logo"/>
+<img src= "images/logo.jpg" width="100px" height="50px" alt="logo" align="left"/>
 );
 }
 export default Logo;
